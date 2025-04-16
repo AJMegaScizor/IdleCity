11e7b37d110b0099ca43e2a1e26ce43a9db72df8
Squidking11 <nicobutler4854@gmail.com>
2025-04-16 14:13:45 -0400

    might have fixed startup? idk

--- html/js/script.js
+++ html/js/script.js
@@ -2,20 +2,28 @@ let rate = 0;
 let happiness = 0;
 var KEY = 'changeMoney';
 var num = localStorage[KEY];
-const plotArray = new Array(16);
+let parsedArray = JSON.parse(localStorage.getItem("plotArray"));
+if (parsedArray) {
+    plotArray = parsedArray.map(obj => new plot(obj.building, obj.moneyGen, obj.happiness, obj.plotNum));
+} else {
+    plotArray = new Array(16);
+}
 
 
 function startButton () {
     for (let i = 0; i < plotArray.length; i++) {
         plotArray[i] = new plot("images/apartment.png", 0, 0, i)
     }
+    console.log(plotArray[0].getBuilding());
+    localStorage.setItem("plotArray", JSON.stringify(plotArray));
     console.log("something");
 }
 
 function setImage(plotNum) {
     var id = "b" + plotNum;
     const element = document.getElementById(id);
-    element.src=String(plotArray[plotNum].getBuilding());
+    element.src=plotArray[1].building();
+    console.log(plotArray[1].buidling);
 
 }
 
