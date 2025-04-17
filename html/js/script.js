f7a54aa548db3b15af9fc8c52f3dcca6686c9810
Squidking11 <nicobutler4854@gmail.com>
2025-04-16 22:25:14 -0400

    so much javascript
    can now build "bank"
    it will affect the rate of earning money
    also costs money to build
    starts game with $100

--- html/js/script.js
+++ html/js/script.js
@@ -1,30 +1,52 @@
+class plot {
+    building;
+    moneyGen;
+    happiness;
+    constructor (building, moneyGen, happiness, plotNum) {
+        this.building = building;
+        this.moneyGen = moneyGen;
+        this.happiness = happiness;
+    }
+    getBuilding() {
+        return this.building;
+    }
+    bank() {
+        this.building = "images/apartment.png";
+        this.moneyGen = 5;
+        this.happiness = 1;
+
+    }
+}
+
 let rate = 0;
 let happiness = 0;
 var KEY = 'changeMoney';
 var num = localStorage[KEY];
 let parsedArray = JSON.parse(localStorage.getItem("plotArray"));
+let tempPlotNum = localStorage.getItem("tempPlotNum");
 if (parsedArray) {
-    plotArray = parsedArray.map(obj => new plot(obj.building, obj.moneyGen, obj.happiness, obj.plotNum));
+    plotArray = parsedArray.map(obj => new plot(obj.building, obj.moneyGen, obj.happiness));
 } else {
     plotArray = new Array(16);
 }
 
 
 function startButton () {
+    localStorage.clear();
     for (let i = 0; i < plotArray.length; i++) {
-        plotArray[i] = new plot("images/apartment.png", 0, 0, i)
+        plotArray[i] = new plot("images/emptylot.png", 0, 0)
     }
     console.log(plotArray[0].getBuilding());
     localStorage.setItem("plotArray", JSON.stringify(plotArray));
     console.log("something");
+    localStorage[KEY] = 100;
+    window.location.href="index.html";
 }
 
 function setImage(plotNum) {
     var id = "b" + plotNum;
     const element = document.getElementById(id);
-    element.src=plotArray[1].building();
-    console.log(plotArray[1].buidling);
-
+    element.src=plotArray[plotNum].getBuilding();
 }
 
 function changeMoney() {
@@ -32,6 +54,10 @@ function changeMoney() {
     num = localStorage[KEY];
     if (!num) {
         num = 0;
+    }
+    rate = 0;
+    for (const p of plotArray) {
+        rate += p.moneyGen;
     }
     const element = document.getElementById("money");
     if (element) {
@@ -42,24 +68,25 @@ function changeMoney() {
         localStorage[KEY] = num
     }
 }
+
+function upgrade(plotNum) {
+    tempPlotNum = plotNum;
+    localStorage.setItem("tempPlotNum", plotNum);
+    window.location.href="shop.html";
+}
+function clearPlotNum() {
+    localStorage.setItem("tempPlotNum", null);
+}
+function buildBank() {
+    if (localStorage[KEY] < 100) {
+        localStorage.setItem("tempPlotNum", null);
+        return;
+    }
+    localStorage[KEY] -= 100;
+    plotArray[tempPlotNum].bank();
+    localStorage.setItem("plotArray", JSON.stringify(plotArray));
+    localStorage.setItem("tempPlotNum", null);
+    window.location.href = "index.html";
+}
 //localStorage.clear();
 setInterval(changeMoney, 1000);
-function buildBank(img) {
-    document.getElementById("img").src="images/bread.png";
-}
-
-class plot {
-    building;
-    moneyGen;
-    happiness;
-    plotNum;
-    constructor (building, moneyGen, happiness, plotNum) {
-        this.building = building;
-        this.moneyGen = moneyGen;
-        this.happiness = happiness;
-        this.plotNum = plotNum;
-    }
-    getBuilding() {
-        return this.building;
-    }
-}
